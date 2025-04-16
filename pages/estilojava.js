2bf969c5705f4978a7efcb6af747a7655ef622db
Yamila Palloni <ypalloni@gmail.com>
2025-04-16 15:45:13 -0300

    version5.0

---
+++ pages/estilojava.js
@@ -0,0 +1,23 @@
+
+window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.navbar-custom');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+
+document.querySelectorAll('a.nav-link[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            window.scrollTo({
+                top: target.offsetTop - 80, // ajustá si tenés navbar fija
+                behavior: 'smooth'
+            });
+        }
+    });
+});
